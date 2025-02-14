508e31ce55b35d3a118af95601e5cb3127388108
Dhulipudi-Chaitanya <chaitanya.d2007@gmail.com>
2025-02-14 22:30:35 +0530

    Selection_Sort.c

--- Selection_Sort.c
+++ Selection_Sort.c
@@ -5,49 +5,35 @@
 //If we take an array finding the min and min_Index and if the a[i] > min then swap them
 /*=================================================================================================================================================================================*/
 
-
 #include<stdio.h>
-
-// void selection(int a[],int n){
-// 	int i,j;
-// 	for(i=0;i<n-1;i++){
-//         int c=a[i],e=0;
-// 		for(j=i+1;j<n;j++){   
-// 			if(c>a[j]){
-// 				c=a[j];
-// 				e=j;
-// 			}
-// 		}
-// 		if(a[i]>c)
-// 		{
-// 			int t=a[i];
-// 			a[e]=t;
-// 			a[i]=c;
-// 		}
-// 	}
-
-// }
-// int main(){
-// 	int n;
-// 	scanf("%d",&n);
-// 	int a[n],i;
-// 	for(i=0;i<n;i++){
-// 		scanf("%d",&a[i]);
-// 	}
-// 	selection(a,n);
-// 	printf("after selction sort\n");
-// 	for(i=0;i<n;i++){
-// 		printf("%d ",a[i]);
-// 	}
-// }
-
-#include<math.h>
+int selection_Sort(int a[],int n){
+    int min,min_index;
+    for(int i=0;i<n-1;i++){
+        min=a[i];
+        for(int j=i+1;j<n;j++){ //finding the min value
+            if(min>a[j]){
+                min=a[j];
+                min_index=j;  // store the min_index value
+            }
+        }
+        if(a[i]>min){    //swap
+            int temp=a[min_index];
+            a[min_index]=a[i];
+            a[i]=temp;
+        }
+    }
+}
 int main(){
     int n;
     scanf("%d",&n);
-    
-
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    selection_Sort(a,n);   //calling function
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
 }
-
 
 
