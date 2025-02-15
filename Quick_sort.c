8b5af0667dd092f4233ff5e038ce14cd3ee241b9
Dhulipudi-Chaitanya <chaitanya.d2007@gmail.com>
2025-02-15 07:24:17 +0530

    Quick_sort.c

--- Quick_sort.c
+++ Quick_sort.c
@@ -1,7 +1,12 @@
+
+ // Time Complexity  Best Case: O(n log n)
+//                   Average Case: O(n log n)
+//                   Worst Case: O(n^2)
+
 #include<stdio.h>
  int partation(int a[],int n,int key,int start,int end){
     int s=start,e=end;
-    while(s<e){
+    while(s<e){   
         while(a[s]<=key){
             s++;
         }
@@ -17,15 +22,15 @@
     int temp=a[e];
     a[e]=key;
     a[start]=temp;
-    return e;
+    return e;  // where key index is stored
 }
  void Quick_Sort(int a[],int n,int start,int end){
     
-    if(start<end){
-        int p=a[start];
-        p=partation(a,n,p,start,end);
-        Quick_Sort(a,n,start,p-1);
-        Quick_Sort(a,n,p+1,end);
+    if(start<end){     //  0< n-1
+        int p=a[start];   // p=a[0]
+        p=partation(a,n,p,start,end); // to find the index of the a[start]
+        Quick_Sort(a,n,start,p-1);  // dividing them into furthur single arrays
+        Quick_Sort(a,n,p+1,end);  // dividing them into furthur single arrays
     }
    
 }
@@ -38,7 +43,7 @@ int main(){
     }
     int start=0;
     int end=n-1;
-    Quick_Sort(a,n,start,end);
+    Quick_Sort(a,n,start,end);// function calling
     for(int i=0;i<n;i++){
         printf("%d ",a[i]);
     }
