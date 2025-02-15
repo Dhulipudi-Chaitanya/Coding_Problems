7877c77d829f3844758d9daff6e65e29fd80aba0
Dhulipudi-Chaitanya <chaitanya.d2007@gmail.com>
2025-02-15 07:18:48 +0530

    MERGE_SORT.c

--- MERGE_SORT.c
+++ MERGE_SORT.c
@@ -1,10 +1,16 @@
+/*=================================================================================================================================================================================*/
+        // Time complextiy   Best Case: O(n log n)
+                            //Average Case: O(n log n)
+                            //Worst Case: O(n log n)
+
+/*=================================================================================================================================================================================*/
 void Merge(int a[],int n, int low,int mid,int high){
     int i=low;
     int j=mid+1;
-    int B[n];
+    int B[n]; // we have to add into a new array
     int k=0;
-    while(i<=mid && j<=high){
-        if(a[i]<=a[j]){
+    while(i<=mid && j<=high){ 
+        if(a[i]<=a[j]){ 
             B[k]=a[i];
             k++;
             i++;
@@ -23,22 +29,20 @@ void Merge(int a[],int n, int low,int mid,int high){
         B[k]=a[j];
         k++;j++;
     }
-    for(int i=low;i<=high;i++){
+    for(int i=low;i<=high;i++){  // to change their indexs
         a[i]=B[i];
     }
 }
-
 void Merge_Sort(int a[],int n,int low,int high){
     //Base case where i and j are crossover or equals
     if(low>=high) return ;
-    // real-case
-    int mid=(low+high)/2;
+    // real-case   
+    int mid=(low+high)/2; // finding the mid and dividing them into furthur single array
     Merge_Sort(a,n,low,mid);
     Merge_Sort(a,n,mid+1,high);
     //To merge the arrays  
-    Merge(a,n,low,mid,high);
+    Merge(a,n,low,mid,high);    //To merge the arrays  
 }
-
 #include<stdio.h>
 int main(){
     int n;
@@ -47,7 +51,7 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    Merge_Sort(a,n,0,n-1);
+    Merge_Sort(a,n,0,n-1);// calling function
     for(int i=0;i<n;i++){
         printf("%d ",a[i]);
     }
